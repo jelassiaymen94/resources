8e90c45ee8681349a65c359f08407b5eaf979820
polarmonkey1381 <127989249+polarmonkey1381@users.noreply.github.com>
2023-03-15 14:35:36 -0700

    deliveries

--- [paid]/Renewed-Deliveries/shared/server.lua
+++ [paid]/Renewed-Deliveries/shared/server.lua
@@ -2,9 +2,9 @@ Config = Config or {}
 
 Config.RenewedBanking = true
 Config.PriceBrackets = {
-    ["small"] = 200,
+    ["small"] = 250,
     ["medium"] = 250,
-    ["large"] = 350,
+    ["large"] = 250,
 }
 
 Config.MaxMembers = 4 -- How many people can be in a group during the runs?
@@ -15,7 +15,7 @@ Config.GroupPay = 1.5 -- How much more you get paid for doing a group run (5% mo
 Config.GroupPayLimit = 3 -- How many people to get the Config.GroupPay bonus?
 
 Config.Buffs = true -- Do u use ps or tnj buffs then enable this
-Config.BuffExport = "qb-buffs" -- Some people still use tnj-buffs some uses qb-buffs they are the just edit the BuffExport to whatever you use
+Config.BuffExport = "ps-buffs" -- Some people still use tnj-buffs some uses ps-buffs they are the just edit the BuffExport to whatever you use
 Config.BuffType = "luck" -- What buff type do u use for players to recieve more money?
 Config.BuffPay = 1.15 -- How much more do they get paid for having the buff? (10% more)
 
