6f8004a9647b9d2d270b3c792ea198edc5c5595b
blakeayye <bcoram04@gmail.com>
2023-03-22 00:47:00 -0400

    w

--- [polar]/Polar-Sub/client/taco.lua
+++ [polar]/Polar-Sub/client/taco.lua
@@ -1,3 +1,4 @@
+local QBCore = exports['qb-core']:GetCoreObject()
 CreateThread(function()
    -- while true do 
     --    Wait(1000)
@@ -15,9 +16,9 @@ CreateThread(function()
 			event = "Polar-Sub:client:TacoTruck",
 			icon = "fas fa-bolt",
 			label = "Open Trunk",
-            job = 'taco',
-			canInteract = function(e)local p = GetEntityModel(e) Wait(50) if p == 1951180813 then return true end 
-			return #(GetEntityCoords(PlayerPedId()) - GetOffsetFromEntityInWorldCoords(e, 0.0, -4.0, 0.0)) < 5.0 end
+           -- job = 'taco',
+			canInteract = function(e) local p = GetEntityModel(e) Wait(50) if p == 1951180813 then return true end end
+			
 			
 		}
 	},
@@ -25,4 +26,39 @@ CreateThread(function()
 	})
    -- end
 end)
-RegisterNetEvent('Polar-Sub:client:TacoTruck', function() print('TACO!')  end)
+function LoadAnimDict(dict)
+    if HasAnimDictLoaded(dict) then return end
+
+    RequestAnimDict(dict)
+    while not HasAnimDictLoaded(dict) do
+        Wait(10)
+    end
+end
+RegisterNetEvent('Polar-Sub:client:TacoTruck', function() 
+	LoadAnimDict("amb@prop_human_bum_bin@idle_b")
+    TaskPlayAnim(PlayerPedId(), "amb@prop_human_bum_bin@idle_b", "idle_d", 4.0, 4.0, -1, 50, 0, false, false, false)
+	exports['qb-menu']:openMenu({
+        { header = "<img src=nui://"..Config.img..QBCore.Shared.Items['largejoint'].image.." width=45px onerror='this.onerror=null; this.remove();'> Joints",
+        txt = "", isMenuHeader = true },
+        
+		{ header = "<img src=nui://"..Config.img..QBCore.Shared.Items['1ozhigh'].image.." width=45px onerror='this.onerror=null; this.remove();'> Small Joint",
+        txt = "Roll Some Small Ass Joints (1 Ounce Needed)", params = { event = "Polar-Weed:Client:SmallJointMake",}},
+        
+		{ header = "<img src=nui://"..Config.img..QBCore.Shared.Items['1ozhigh'].image.." width=45px onerror='this.onerror=null; this.remove();'> Large Joint",
+        txt = "fuk u", params = { event = "Polar-Sub:Client:Make2",}},
+        
+		{ header = "Exit", params = { event = "Polar-Sub:client:CloseTruck"}},
+    })
+end)
+RegisterNetEvent('Polar-Sub:client:CloseTruck', function ()
+	LoadAnimDict("amb@prop_human_bum_bin@idle_b")
+    TaskPlayAnim(PlayerPedId(), "amb@prop_human_bum_bin@idle_b", "exit", 4.0, 4.0, -1, 50, 0, false, false, false)
+
+
+end)
+RegisterNetEvent('Polar-Sub:Client:Make2', function()
+	LoadAnimDict("amb@prop_human_bum_bin@idle_b")
+    TaskPlayAnim(PlayerPedId(), "amb@prop_human_bum_bin@idle_b", "exit", 4.0, 4.0, -1, 50, 0, false, false, false)
+	print('2')
+
+end)
