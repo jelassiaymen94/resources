6607c03146c4dce3a56c7395fddfbdd935d2e28a
blakeayye <bcoram04@gmail.com>
2023-05-12 17:26:57 -0400

    1

---
+++ [qb]/qb-grandmas/config.lua
@@ -0,0 +1,8 @@
+Config = Config or {}
+
+
+Config.CheckBalance = true
+Config.CheckDead = true
+Config.Coords = vector4(2812.18, 5977.55, 350.08, 12.71)
+Config.Cost = 500
+Config.MoneyType = 'cash'
