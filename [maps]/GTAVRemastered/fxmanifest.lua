8de97f8695e747339425061e56fb8cf4452f2f7a
Nezarecc <134020196+Nezarecc@users.noreply.github.com>
2023-07-24 18:37:42 -0400

    update

---
+++ [maps]/GTAVRemastered/fxmanifest.lua
@@ -0,0 +1,11 @@
+-- This mod was made by Larcius.
+-- For more information check out https://www.patreon.com/larcius
+
+fx_version "adamant"
+game "gta5"
+
+author 'Larcius'
+description 'GTA V Remastered: Ultimate'
+version '4.4'
+
+data_file('DLC_ITYP_REQUEST')('stream/vremastered_slod.ytyp')
