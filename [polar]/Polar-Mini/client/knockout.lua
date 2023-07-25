88cfcf99b8313558719ca32283b12c981c534418
Nezarecc <134020196+Nezarecc@users.noreply.github.com>
2023-07-24 23:00:56 -0400

    1

--- [polar]/Polar-Mini/client/knockout.lua
+++ [polar]/Polar-Mini/client/knockout.lua
@@ -5,12 +5,13 @@ local isdead = false
 
 CreateThread(function()
         while true do
-            Wait(1)
+            Wait(100)
             local ped = PlayerPedId()
             PlayerData = QBCore.Functions.GetPlayerData()
             if not knockedOut then
             if IsPedInMeleeCombat(ped) then
                 if (HasPedBeenDamagedByWeapon(ped, GetHashKey("WEAPON_UNARMED"), 0)) then
+                    print(count)
                     count += 1
                    if count > 2 then
                     if GetEntityHealth(ped) < 160 then
--- [polar]/Polar-Mini/client/t_1.lua
+++ [polar]/Polar-Mini/client/t_1.lua
@@ -1,40 +1,4 @@
 
-local type = 'mph' -- mph or kmh, sets type of speed your server uses
 
 
-local sleeptimer = nil local vroom = nil local stop = nil
 
-
-RegisterCommand('startspeed', function()
-    if GetVehiclePedIsIn(PlayerPedId(), false) ~= 0 then
-        CreateThread(function()
-            stop = false
-            while true do
-                if stop then break end
-                if GetVehiclePedIsIn(PlayerPedId(), false) ~= 0 then
-                local car = GetVehiclePedIsIn(PlayerPedId()) local model = GetEntityModel(car)  local m = GetDisplayNameFromVehicleModel(model)
-                local speed = GetEntitySpeed(PlayerPedId()) -- PlayerPedId()
-                if type == 'mph' then
-                    vroom = speed * 2.236936 -- mph
-                    print('Your Speed is ' .. math.floor(vroom) .. ' Miles Per Hour in a ' .. m .. '')
-                elseif type == 'kmh' then
-                    vroom = speed * 3.6
-                    print('Your Speed is ' .. math.floor(vroom) .. ' Kilometers Per Hour in a ' .. m .. '')
-                else
-                    print('wrong type')
-                end
-                time = sleeptimer or 2000
-                Wait(time)
-                else break end
-            end
-        end)
-    else print('no car') end
-end)
-RegisterCommand('setspeed', function(source, args)
-        sleeptime = tonumber(args[1])
-        sleeptimer = sleeptime * 1000
-end)
-RegisterCommand('stopspeed', function()
-    stop = true
-    sleeptimer = nil
-end)
