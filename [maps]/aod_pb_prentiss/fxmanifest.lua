3ec6f40e4db797dcb402d05412d62ee1ab162e0a
blakeayye <bcoram04@gmail.com>
2023-07-25 12:22:59 -0400

    1

---
+++ [maps]/aod_pb_prentiss/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+description 'Angels of Death clubhouse PALETO BAY VERSION'
+author 'Prentiss - https://discord.gg/Kze4pBreVz'
+-- I just want to say thank you for supporting me
+-- Dont forget to join in my Discord for more information, support and free stuff 
+this_is_a_map 'yes'
+dependency '/assetpacks'
