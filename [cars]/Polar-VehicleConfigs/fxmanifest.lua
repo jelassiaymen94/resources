d823765d11c9b93bc40185e022268b5be5f36e49
blakeayye <bcoram04@gmail.com>
2023-07-25 23:09:56 -0400

    1

--- [cars]/Polar-VehicleConfigs/fxmanifest.lua
+++ [cars]/Polar-VehicleConfigs/fxmanifest.lua
@@ -7,13 +7,14 @@ data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 data_file 'CARCOLS_FILE' 'carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
 --data_file 'WEAPONINFO_FILE' 'additional/weapon_dish.meta'
-
+data_file 'HANDLING_FILE' 'Unified-handling.meta'
 files {
   'vehiclelayouts.meta',
   'handling.meta',
   'vehicles.meta',
   'carcols.meta',
   'carvariations.meta',
+  'Unified-handling.meta',
 --  'additional/weapon_dish.meta',
 }
 
