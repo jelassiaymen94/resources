3a4113381404929a4f09db0ce56164ff0ec0f6cd
blakeayye <bcoram04@gmail.com>
2023-07-28 23:41:54 -0400

    1

---
+++ [polar]/Polar-Store/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Polar'
+description 'Store'
+version '1.0.1'
+lua54 'yes'
+this_is_a_map 'yes'
+
+files {
+      'polar_ls_store.ytyp'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'polar_ls_store.ytyp'
+
+dependency '/assetpacks'
