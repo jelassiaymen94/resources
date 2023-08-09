b55f024a76fa3411f9bdf6d0faf13fce8187a1c4
blakeayye <bcoram04@gmail.com>
2023-08-09 07:03:02 -0400

    1

--- [qb]/qb-apartments/server/main.lua
+++ [qb]/qb-apartments/server/main.lua
@@ -27,7 +27,27 @@ local function GetApartmentInfo(apartmentId)
 end
 
 -- Events
-
+RegisterNetEvent('Polar-Apartments:Server:Start', function()
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    for _, v in pairs(QBCore.Shared.StarterItems) do
+        local info = {}
+        if v.item == "id_card" then
+            info.citizenid = Player.PlayerData.citizenid
+            info.firstname = Player.PlayerData.charinfo.firstname
+            info.lastname = Player.PlayerData.charinfo.lastname
+            info.birthdate = Player.PlayerData.charinfo.birthdate
+            info.gender = Player.PlayerData.charinfo.gender
+            info.nationality = Player.PlayerData.charinfo.nationality
+        elseif v.item == "driver_license" then
+            info.firstname = Player.PlayerData.charinfo.firstname
+            info.lastname = Player.PlayerData.charinfo.lastname
+            info.birthdate = Player.PlayerData.charinfo.birthdate
+            info.type = "Class C Driver License"
+        end
+        Player.Functions.AddItem(v.item, v.amount, false, info)
+    end
+end)
 RegisterNetEvent('qb-apartments:server:SetInsideMeta', function(house, insideId, bool, isVisiting)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
