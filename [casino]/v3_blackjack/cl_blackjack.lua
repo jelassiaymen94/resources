c33b2e00049848ecfaf75fdc44bdc0d08975a731
blakeayye <bcoram04@gmail.com>
2023-08-09 08:45:16 -0400

    rrrr

--- [casino]/v3_blackjack/cl_blackjack.lua
+++ [casino]/v3_blackjack/cl_blackjack.lua
@@ -453,30 +453,27 @@ RegisterNetEvent('Polar-Casino:Client:BlackJackHandle', function()
             header = "Mini Bet",
             txt = "Bet 100 Chips!",
             icon = "fa-solid fa-coins",
-            amount = 100,
             params = {
                 event = "Polar-Casino:Client:MaxBet",
-                
+                args = {amount = 100},
             }
         },
         {
             header = "Medium Bet",
             txt = "Bet 500 Chips!",
             icon = "fa-solid fa-coins",
-            amount = 500,
             params = {
                 event = "Polar-Casino:Client:MaxBet",
-                
+                args = {amount = 500},
             }
         },
         {
             header = "Max Bet",
             txt = "Bet 2,000 Chips!",
             icon = "fa-solid fa-coins",
-            amount = 2000,
             params = {
                 event = "Polar-Casino:Client:MaxBet",
-                
+                args = {amount = 2000},
             }
         },
         {
