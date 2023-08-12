c13b75586a8358c3cb0c968c22f6b46419e8ffe0
blakeayye <bcoram04@gmail.com>
2023-08-11 22:42:11 -0400

    1

--- [polar]/Polar-Paleto/client/client.lua
+++ [polar]/Polar-Paleto/client/client.lua
@@ -411,7 +411,7 @@ function grabloot(door, object)
     loadModel(bagcolor)
     
     local sceneObject = GetClosestObjectOfType(trolylp[door], 2.0, trollys[door], 0, 0, 0)
-    
+    print(sceneObject)
     while not NetworkHasControlOfEntity(sceneObject) do
         Wait(1)
         --print('error')
@@ -419,14 +419,14 @@ function grabloot(door, object)
     end
 
     local bag = CreateObject(GetHashKey(bagcolor), pedCoords, true, false, false)
-    local scene1 = NetworkCreateSynchronisedScene(GetEntityCoords(object2), GetEntityRotation(object2), 2, true, false, 1065353216, 0, 1.3)
+    local scene1 = NetworkCreateSynchronisedScene(GetEntityCoords(sceneObject), GetEntityRotation(sceneObject), 2, true, false, 1065353216, 0, 1.3)
     NetworkAddPedToSynchronisedScene(PlayerPedId(), scene1, animDict, 'intro', 1.5, -4.0, 1, 16, 1148846080, 0)
     NetworkAddEntityToSynchronisedScene(bag, scene1, animDict, 'bag_intro', 4.0, -8.0, 1)
-    local scene2 =  NetworkCreateSynchronisedScene(GetEntityCoords(object2), GetEntityRotation(object2), 2, true, false, 1065353216, 0, 1.3)
+    local scene2 =  NetworkCreateSynchronisedScene(GetEntityCoords(sceneObject), GetEntityRotation(sceneObject), 2, true, false, 1065353216, 0, 1.3)
     NetworkAddPedToSynchronisedScene(PlayerPedId(), scene2, animDict, 'grab', 1.5, -4.0, 1, 16, 1148846080, 0)
     NetworkAddEntityToSynchronisedScene(bag, scene2, animDict, 'bag_grab', 4.0, -8.0, 1)
-    NetworkAddEntityToSynchronisedScene(object2, scene2, animDict, 'cart_cash_dissapear', 4.0, -8.0, 1)
-    local scene3 =  NetworkCreateSynchronisedScene(GetEntityCoords(object2), GetEntityRotation(object2), 2, true, false, 1065353216, 0, 1.3)
+    NetworkAddEntityToSynchronisedScene(sceneObject, scene2, animDict, 'cart_cash_dissapear', 4.0, -8.0, 1)
+    local scene3 =  NetworkCreateSynchronisedScene(GetEntityCoords(sceneObject), GetEntityRotation(sceneObject), 2, true, false, 1065353216, 0, 1.3)
     NetworkAddPedToSynchronisedScene(PlayerPedId(), scene3, animDict, 'exit', 1.5, -4.0, 1, 16, 1148846080, 0)
     NetworkAddEntityToSynchronisedScene(bag, scene3, animDict, 'bag_exit', 4.0, -8.0, 1)
     NetworkStartSynchronisedScene(scene1)
