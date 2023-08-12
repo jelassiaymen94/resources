d4273d3cea2a0053b9e5ca7a9a363590394febdc
blakeayye <bcoram04@gmail.com>
2023-08-12 05:14:50 -0400

    1

--- [paid]/pug-sling/server.lua
+++ [paid]/pug-sling/server.lua
@@ -13,47 +13,14 @@ local QBCore = exports['qb-core']:GetCoreObject()
 
 
 local function checkItems(Player, items)
-	print('1')
 	if Player == nil then return end
-	if type(items) == 'table' then
-		local count = 0
-		local finalcount = 0
-		for k, v in pairs(items) do
-			if type(k) == 'string' then
-				finalcount = 0
-				for _ in pairs(items) do finalcount += 1 end
-				local item = Player.Functions.GetItemByName(k)
-				if item then
-					if item.amount >= v then
-						
-							return true
-						
-					end
-				end
-			else
-				finalcount = #items
-				local item = Player.Functions.GetItemByName(v)
-				if item then
-					if needsAll then
-						count += 1
-						if count == finalcount then
-							
-							return true
-						end
-					else
-						
-						return true
-					end
-				end
-			end
-		end
-	else
+	
 		local item = Player.Functions.GetItemByName(items)
 		if item then
 			
 			return true
 		end
-	end
+	
 	return false
 end
 
@@ -68,8 +35,17 @@ end
 
 
 QBCore.Functions.CreateCallback('pug-sling:server:checkItems', function(source, cb, items)
-	local Player = QBCore.Functions.GetPlayer(source)
-	cb(checkItems(Player, items))
+	local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    
+
+    for a, b in pairs(Player.PlayerData.items) do
+    if b.name == items then
+    	cb(true)
+	else
+		cb(false)
+	end
+	end
 end)
 
 
