7500fa3e709e285268baac853f4dd5c7837821a8
blakeayye <bcoram04@gmail.com>
2023-08-12 15:25:40 -0400

    1

--- [polar]/Polar-Stores/shared/sh_props.lua
+++ [polar]/Polar-Stores/shared/sh_props.lua
@@ -70,99 +70,65 @@ Config.Cash = {
     'prop_cash_pile_02',
     'prop_anim_cash_pile_01',
     'prop_anim_cash_pile_02', 
-    'bkr_prop_bkr_cash_roll_01'
+    'bkr_prop_bkr_cash_roll_01',
+
+
+    'prop_amb_donut',
+    'prop_donut_02',
+    'prop_food_cb_donuts',
+    'prop_choc_pq',
+
+    'v_res_tt_cigs01',
+    'prop_cigar_02',
+    'prop_cs_ciggy_01',
+    'sf_prop_sf_art_box_cig_01a',
+
+    'prop_vodka_bottle',
+    'prop_beer_am',
+    'prop_cs_beer_bot_02',
+    'prop_amb_beer_bottle',
+    'v_ret_ml_beerpis1',
+    'prop_whiskey_bottle',
+    'prop_wine_bot_01',
+    'prop_wine_bot_02',
+    'prop_wine_rose',
+
+    'v_res_tt_cereal02',
+    'v_res_fa_cereal02',
+    'v_res_fa_cereal01',
+
+    'bkr_prop_coke_bakingsoda',
+
+    'ng_proc_sodacup_01c',
+    'ng_proc_sodacup_01a',
+    'ng_proc_sodabot_01a',
+    'ng_proc_sodacan_01b',
+    'ng_proc_sodacan_01a',
+
+    'v_ret_247_tomsoup1',
+    'v_ret_247_vegsoup1',
+    'v_res_fa_tintomsoup',
+
+    'xs_prop_chips_tube_wl',
+    'ng_proc_food_chips01a',
+    'v_res_fa_shoebox3',
+    'xm3_prop_xm3_pineapple_01a',
+    'sf_prop_sf_apple_01a',
+ 
+
 }
 Config.Gold = {
-    'ch_prop_gold_bar_01a', 
-    'hei_prop_heist_gold_bar', 
-    'prop_gold_bar'
+    'vw_prop_vw_coin_01a',
+    'vw_prop_plaq_1kdollar_x1',
+    'vw_prop_chip_1kdollar_st',
+    'vw_prop_chip_100dollar_st',
 }
 Config.Special = {
     'ex_prop_exec_award_gold',
-    'vw_prop_casino_art_bird_01a',
-    'vw_prop_art_pug_02a',
-    'vw_prop_casino_art_horse_01a',
-    'vw_prop_casino_art_panther_01c',
-    'vw_prop_miniature_yacht_01a',
-    'vw_prop_miniature_yacht_01b',
-    'vw_prop_miniature_yacht_01c',
-    'vw_prop_vw_chips_pile_02a',
-    'vw_prop_vw_chips_pile_01a',
-}
-Config.ShelfSpecial = {
-    'prop_peyote_gold_01', 
+    'prop_peyote_gold_01',
     'xs_prop_trophy_goldbag_01a',
     'vw_prop_vw_pogo_gold_01a',
     'h4_prop_h4_gold_coin_01a',
     'vw_prop_casino_art_egg_01a',
-    'vw_prop_casino_art_miniature_09a',
-    'vw_prop_casino_art_miniature_09b',
-    'vw_prop_casino_art_miniature_09c',
-    'vw_prop_casino_art_miniature_05a',
-    'vw_prop_casino_art_miniature_05b',
-    'vw_prop_casino_art_miniature_05c',
-    'vw_prop_casino_art_car_09a',
-    'vw_prop_casino_art_car_11a',
-    'vw_prop_casino_art_grenade_01a',
-    'vw_prop_casino_art_grenade_01b',
-    'vw_prop_casino_art_grenade_01c',
-    'vw_prop_casino_art_basketball_02a',
-    'v_res_r_fighorse',
-    'v_res_r_fighorsestnd',
-    'ch_prop_ch_trophy_monkey_01a',
-    'ch_prop_ch_trophy_racer_01a',
-    'ch_prop_ch_trophy_strife_01a',
-    'vw_prop_casino_art_skull_01b',
-    'vw_prop_casino_art_skull_02b',
-    'vw_prop_casino_art_figurines_01a',
-    'vw_prop_casino_art_figurines_02a',
-    'vw_prop_casino_art_lampf_01a',
-    'vw_prop_toy_sculpture_01a'
-   
-}
 
-
-
-Config.Paintings = {
-    'ch_prop_vault_painting_01a',
-    'ch_prop_vault_painting_01b',
-    'ch_prop_vault_painting_01c',
-    'ch_prop_vault_painting_01d',
-    'ch_prop_vault_painting_01e',
-    'ch_prop_vault_painting_01f',
-    'ch_prop_vault_painting_01g',
-    'ch_prop_vault_painting_01h',
-    'ch_prop_vault_painting_01i',
-    'ch_prop_vault_painting_01j',
-    'h4_prop_h4_painting_01a',
-    'h4_prop_h4_painting_01b',
-    'h4_prop_h4_painting_01c',
-    'h4_prop_h4_painting_01d',
-    'h4_prop_h4_painting_01e',
-    'h4_prop_h4_painting_01f',
-    'h4_prop_h4_painting_01g',
-    'h4_prop_h4_painting_01h',
-}
-
-
-Config.Cases = {
-    'h4_prop_h4_art_pant_01a',
-    'h4_prop_h4_diamond_01a',
-    'h4_prop_h4_necklace_01a',
-    'h4_prop_h4_t_bottle_02a',
-    'h4_prop_h4_t_bottle_02b',
-    'h4_prop_h4_t_bottle_01a',
-}
-
-Config.PaintAnimations = {
-    {"top_left_enter", "top_left_enter_ch_prop_ch_sec_cabinet_02a", "top_left_enter_ch_prop_vault_painting_01a", "top_left_enter_hei_p_m_bag_var22_arm_s", "top_left_enter_w_me_switchblade"},
-    {"cutting_top_left_idle", "cutting_top_left_idle_ch_prop_ch_sec_cabinet_02a", "cutting_top_left_idle_ch_prop_vault_painting_01a", "cutting_top_left_idle_hei_p_m_bag_var22_arm_s", "cutting_top_left_idle_w_me_switchblade"},
-    {"cutting_top_left_to_right", "cutting_top_left_to_right_ch_prop_ch_sec_cabinet_02a", "cutting_top_left_to_right_ch_prop_vault_painting_01a", "cutting_top_left_to_right_hei_p_m_bag_var22_arm_s", "cutting_top_left_to_right_w_me_switchblade"},
-    {"cutting_top_right_idle", "_cutting_top_right_idle_ch_prop_ch_sec_cabinet_02a", "cutting_top_right_idle_ch_prop_vault_painting_01a", "cutting_top_right_idle_hei_p_m_bag_var22_arm_s", "cutting_top_right_idle_w_me_switchblade"},
-    {"cutting_right_top_to_bottom", "cutting_right_top_to_bottom_ch_prop_ch_sec_cabinet_02a", "cutting_right_top_to_bottom_ch_prop_vault_painting_01a", "cutting_right_top_to_bottom_hei_p_m_bag_var22_arm_s", "cutting_right_top_to_bottom_w_me_switchblade"},
-    {"cutting_bottom_right_idle", "cutting_bottom_right_idle_ch_prop_ch_sec_cabinet_02a", "cutting_bottom_right_idle_ch_prop_vault_painting_01a", "cutting_bottom_right_idle_hei_p_m_bag_var22_arm_s", "cutting_bottom_right_idle_w_me_switchblade"},
-    {"cutting_bottom_right_to_left", "cutting_bottom_right_to_left_ch_prop_ch_sec_cabinet_02a", "cutting_bottom_right_to_left_ch_prop_vault_painting_01a", "cutting_bottom_right_to_left_hei_p_m_bag_var22_arm_s", "cutting_bottom_right_to_left_w_me_switchblade"},
-    {"cutting_bottom_left_idle", "cutting_bottom_left_idle_ch_prop_ch_sec_cabinet_02a", "cutting_bottom_left_idle_ch_prop_vault_painting_01a", "cutting_bottom_left_idle_hei_p_m_bag_var22_arm_s", "cutting_bottom_left_idle_w_me_switchblade"},
-    {"cutting_left_top_to_bottom", "cutting_left_top_to_bottom_ch_prop_ch_sec_cabinet_02a", "cutting_left_top_to_bottom_ch_prop_vault_painting_01a", "cutting_left_top_to_bottom_hei_p_m_bag_var22_arm_s", "cutting_left_top_to_bottom_w_me_switchblade"},
-    {"with_painting_exit", "with_painting_exit_ch_prop_ch_sec_cabinet_02a", "with_painting_exit_ch_prop_vault_painting_01a", "with_painting_exit_hei_p_m_bag_var22_arm_s", "with_painting_exit_w_me_switchblade"},
 }
