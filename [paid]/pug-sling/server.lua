44d105003857be902082dde189a40b1c2941a0c7
blakeayye <bcoram04@gmail.com>
2023-08-12 03:36:27 -0400

    1

--- [paid]/pug-sling/server.lua
+++
@@ -1,4 +0,0 @@
-local QBCore = exports[Config.CoreName]:GetCoreObject()
---[[QBCore.Commands.Add("sling", "Toggle weapon sling to back/chest", {}, false, function(source, args)
-	TriggerClientEvent("Pug:sling:togglePos", source)
-end)]]
