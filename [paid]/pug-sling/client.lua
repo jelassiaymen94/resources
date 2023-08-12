aa7bcb85cb77dfdfb51eea0eeca4a92f2070219f
blakeayye <bcoram04@gmail.com>
2023-08-12 03:45:21 -0400

    1

--- [paid]/pug-sling/client.lua
+++ [paid]/pug-sling/client.lua
@@ -1,3 +1,6 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+
 local placementConfig = Config.Placement["Back"]
 RegisterNetEvent('weaponBack:equipWeapon')
 AddEventHandler('weaponBack:equipWeapon', function(weaponHash)
@@ -22,7 +25,7 @@ AddEventHandler('weaponBack:equipWeapon', function(weaponHash)
         end
         
         local weaponObject = CreateObject(GetHashKey(weaponModel), 0.0, 0.0, 0.0, true, true, true)
-        AttachEntityToEntity(weaponObject, playerPed, GetPedBoneIndex(playerPed, placementConfig.bone), placementConfig.x, placementConfig.y, placementConfig.z, placementConfig.x_rotation, placementConfig.y_rotation, placementConfig.z_rotation, true, true, false, true, 1, true)
+        AttachEntityToEntity(weaponObject, PlayerPedId(), GetPedBoneIndex(PlayerPedId(), placementConfig.bone), placementConfig.x, placementConfig.y, placementConfig.z, placementConfig.x_rotation, placementConfig.y_rotation, placementConfig.z_rotation, true, true, false, true, 1, true)
         weaponsOnBack[weaponHash] = weaponObject
         weaponPlacements[weaponHash] = newPlacement
     end
@@ -43,14 +46,14 @@ Citizen.CreateThread(function()
     while true do
         Citizen.Wait(Config.LoopSpeed)
 
-        local playerPed = PlayerPedId()
+      
 
-        if DoesEntityExist(playerPed) and not IsEntityDead(playerPed) then
+        if DoesEntityExist(PlayerPedId()) and not IsEntityDead(PlayerPedId()) then
             local currentWeapons = {}
 
             for i = 1, #Config.WeaponsOnBack do
                 local weaponHash = Config.WeaponsOnBack[i].hash
-                if HasPedGotWeapon(playerPed, weaponHash, false) then
+                if playeritem("weapon_assaultrifle") then
                     table.insert(currentWeapons, weaponHash)
                 end
             end
@@ -61,7 +64,7 @@ Citizen.CreateThread(function()
                     TriggerEvent('weaponBack:equipWeapon', weaponHash)
                 end
             end
-
+            
             for weaponHash, weaponObject in pairs(weaponsOnBack) do
                 if not table.includes(currentWeapons, weaponHash) then
                     DeleteEntity(weaponObject)
@@ -73,3 +76,43 @@ Citizen.CreateThread(function()
         end
     end
 end)
+
+
+
+
+local PlayerData = nil
+function playeritem(items, amount)
+        PlayerData = QBCore.Functions.GetPlayerData()
+        local isTable = type(items) == 'table'
+        local isArray = isTable and table.type(items) == 'array' or false
+        local totalItems = #items
+        local count = 0
+        local kvIndex = 2
+        if isTable and not isArray then
+            totalItems = 0
+            for _ in pairs(items) do 
+                local totalItems2 = totalItems + 1 
+                totalItems = totalItems2
+            end
+            kvIndex = 1
+        end
+        for _, itemData in pairs(PlayerData.items) do
+            if isTable then
+                for k, v in pairs(items) do
+                    local itemKV = {k, v}
+                    if itemData and itemData.name == itemKV[kvIndex] and ((amount and itemData.amount >= amount) or (not isArray and itemData.amount >= v) or (not amount and isArray)) then
+                        local count2 = count + 1 
+                        count = count2
+                    end
+                end
+                if count == totalItems then
+                    return true
+                end
+            else -- Single item as string
+                if itemData and itemData.name == items and (not amount or (itemData and amount and itemData.amount >= amount)) then
+    return true
+                end
+            end
+        end
+    return false
+end
