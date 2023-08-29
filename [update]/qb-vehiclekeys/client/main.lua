508e6c2d9dcd81a4f0fea1e513c83a23eb49e104
blakeayye <bcoram04@gmail.com>
2023-08-29 15:25:51 -0400

    1

--- [update]/qb-vehiclekeys/client/main.lua
+++ [update]/qb-vehiclekeys/client/main.lua
@@ -382,7 +382,44 @@ function GetPedsInVehicle(vehicle)
     end
     return otherPeds
 end
-
+local PlayerData = nil
+function playeritem(items, amount)
+     
+        PlayerData = QBCore.Functions.GetPlayerData()
+       
+        local isTable = type(items) == 'table'
+        local isArray = isTable and table.type(items) == 'array' or false
+        local totalItems = #items
+        local count = 0
+        local kvIndex = 2
+        if isTable and not isArray then
+            totalItems = 0
+            for _ in pairs(items) do 
+                local totalItems2 = totalItems + 1 
+                totalItems = totalItems2
+            end
+            kvIndex = 1
+        end
+        for _, itemData in pairs(PlayerData.items) do
+            if isTable then
+                for k, v in pairs(items) do
+                    local itemKV = {k, v}
+                    if itemData and itemData.name == itemKV[kvIndex] and ((amount and itemData.amount >= amount) or (not isArray and itemData.amount >= v) or (not amount and isArray)) then
+                        local count2 = count + 1 
+                        count = count2
+                    end
+                end
+                if count == totalItems then
+                    return true
+                end
+            else -- Single item as string
+                if itemData and itemData.name == items and (not amount or (itemData and amount and itemData.amount >= amount)) then
+                    return true
+                end
+            end
+        end
+        return false
+end
 function IsBlacklistedWeapon()
     local weapon = GetSelectedPedWeapon(PlayerPedId())
     if weapon ~= nil then
