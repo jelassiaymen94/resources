e0087b5cbf00ababead66a961112c86077d2fbf8
blakeayye <bcoram04@gmail.com>
2023-09-01 15:07:36 -0400

    1

--- [new2]/radio/cl_radio.lua
+++ [new2]/radio/cl_radio.lua
@@ -8,18 +8,33 @@ local waitTime = 100
 
 CreateThread(function()
 	
-	for i = 0, GetNumUnlockedRadioStations() - 1 do
-		radioStations[GetRadioStationName(i)] = 0
-        
-	end
+	
 	SetRadioStationIsVisible("RADIO_04_PUNK", false)
-	
-	
-	while true do
-		Wait(waitTime)
+	--SetRadioStationIsVisible("RADIO_03_HIPHOP_NEW", false)
+	--SetRadioStationIsVisible("RADIO_02_POP", false)
+    SetRadioStationIsVisible("RADIO_01_CLASS_ROCK", false)
+    SetRadioStationIsVisible("RADIO_05_TALK_01", false)
+    --SetRadioStationIsVisible("RADIO_06_COUNTRY", false)
+    SetRadioStationIsVisible("RADIO_07_DANCE_01", false)
+    SetRadioStationIsVisible("RADIO_08_MEXICAN", false)
+    SetRadioStationIsVisible("RADIO_09_HIPHOP_OLD", false)
+    SetRadioStationIsVisible("RADIO_12_REGGAE", false)
+    SetRadioStationIsVisible("RADIO_13_JAZZ", false)
+    SetRadioStationIsVisible("RADIO_14_DANCE_02", false)
+    SetRadioStationIsVisible("RADIO_15_MOTOWN", false)
+    SetRadioStationIsVisible("RADIO_16_SILVERLAKE", false)
+    SetRadioStationIsVisible("RADIO_19_USER", false)
+    SetRadioStationIsVisible("RADIO_18_90S_ROCK", false)
+    SetRadioStationIsVisible("RADIO_17_FUNK", false)
+    SetRadioStationIsVisible("RADIO_20_THELAB", false)
+    SetRadioStationIsVisible("RADIO_11_TALK_02", false)
+    SetRadioStationIsVisible("RADIO_21_DLC_XM17", false)
+    SetRadioStationIsVisible("RADIO_22_DLC_BATTLE_MIX1_RADIO", false)
+	--while true do
+		--Wait(waitTime)
 		
-		SetRadioStationIsVisible("RADIO_02_POP", false)
+		--SetRadioStationIsVisible("RADIO_02_POP", false)
 					
 			
-	end
+	--end
 end)
