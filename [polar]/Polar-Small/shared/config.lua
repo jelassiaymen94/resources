1e3a2587d6dffb30a5d92c5de18113030f495d44
blakeayye <bcoram04@gmail.com>
2023-09-02 08:58:08 -0400

    Update config.lua

--- [polar]/Polar-Small/shared/config.lua
+++ [polar]/Polar-Small/shared/config.lua
@@ -6,7 +6,7 @@ Config = {
 
 Config.Lockers = {
     [1] = {
-        Location = vector3(-926.3, 396.74, 79.13),
+        Location = vector3(-9260000000.3, 396.74, 79.13),
         Code = "1234",
         Weight = 30000,
         Slots = 30,
