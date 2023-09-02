79018395ebe3d87cd1abe898a3ae0f463b4f153e
blakeayye <bcoram04@gmail.com>
2023-09-02 08:49:19 -0400

    1

--- [new2]/Polar-Lockers/config.lua
+++ [new2]/Polar-Lockers/config.lua
@@ -3,8 +3,8 @@ Config = {}
 
 Config.Lockers = {
     [1] = {
-        Location = '',
-        Code = "",
+        Location = vector3(-926.3, 396.74, 79.13),
+        Code = "1234",
         Weight = 30000,
         Slots = 30,
         USESPECIAL = nil, -- either 'gang', 'job', 'cid', -- if none then keep nil
