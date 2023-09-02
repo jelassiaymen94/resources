fca7b15a1e4ddcc078b158aee50e72bcdf5113c3
blakeayye <bcoram04@gmail.com>
2023-09-02 06:46:56 -0400

    Update targets.lua

--- [polar]/Polar-Crafting/client/targets.lua
+++ [polar]/Polar-Crafting/client/targets.lua
@@ -39,9 +39,9 @@ function targets()
     makeProp({prop = `prop_tool_bench02`, coords = vector4(table.x, table.y, table.z, table.w-180.0)}, 1, false)
         
     local n = math.random(1,9999) local z = math.random(1,9999)
-   -- print("name:".. n .. z)
-    exports['qb-target']:AddBoxZone("name:".. n .. z, vector3(table.x, table.y, table.z), 0.8, 0.5, 
-    { name="name:".. n .. z, heading = table.w+180.0, debugPoly=false, minZ=table.z-1.05, maxZ=table.z+0.80 },
+   -- print("name:" .. value .. n .. z)
+    exports['qb-target']:AddBoxZone("name:" .. value .. n .. z, vector3(table.x, table.y, table.z), 0.8, 0.5, 
+    { name="name:" .. value .. n .. z, heading = table.w+180.0, debugPoly=false, minZ=table.z-1.05, maxZ=table.z+0.80 },
 	{ options = { { event = "Polar-Crafting:Client:OpenMenuPublic", icon = "fa-solid fa-bolt", label = "Open", benches = value, minimum = minim }, },
 	distance = 2.0 })
 
