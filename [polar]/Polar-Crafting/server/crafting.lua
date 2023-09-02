4943a9decb8a1c09e6a1a48bf1960775cb02219d
blakeayye <bcoram04@gmail.com>
2023-09-02 09:35:57 -0400

    1

--- [polar]/Polar-Crafting/server/crafting.lua
+++ [polar]/Polar-Crafting/server/crafting.lua
@@ -1,62 +1,9 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-local src = source
 
 
 
 
-
-
-RegisterNetEvent('Polar-Crafting:Server:IncreaseXp', function(amount)
-    local inc = amount
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-  
-    
-
-   
-   
-
-end)
-RegisterNetEvent('Polar-Crafting:Server:XpNil', function()
-
-    xp = nil
-
-end)
-
-function next(itemname, xp, give)
- 
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-  
-
-    Wait(100)
-    TriggerClientEvent('Polar-Crafting:Client:StartAnim', src)
-    local packageTime = 15000
-    TriggerClientEvent('Polar-Crafting:Client:Anim', src, itemname)
-    SetTimeout(packageTime, function()
-            TriggerClientEvent('Polar-Crafting:Client:EndAnim', src)
-            TriggerClientEvent('Polar-Crafting:Client:Busy', src)
-            if itemname == 'weapon_fnx45' then
-                local info = {
-                    ammo = 0,
-                    serie="No Serial Number"
-                }
-                Player.Functions.AddItem(itemname, give, nil, info, {["quality"] = 100}) 
-                TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[itemname], "add", give)
-            else
-            Player.Functions.AddItem(itemname, give, nil, {["quality"] = 100}) 
-            TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[itemname], "add", give)
-            end
-            
-            local Player = QBCore.Functions.GetPlayer(src)
-            local data = Player.PlayerData.metadata["craftingrep"] or 0
-            local pp = (data + xp)
-            Player.Functions.SetMetaData('craftingrep', pp)
-            
-    end)
-  
-end   
 
 
 RegisterNetEvent('Polar-Crafting:Server:Hitup', function(data)
@@ -76,7 +23,7 @@ RegisterNetEvent('Polar-Crafting:Server:Hitup', function(data)
     
         if item == nil or item.amount < amount then
             hasRequiredItems = false
-            TriggerClientEvent('QBCore:Notify', src, "You don't have enough materials", 'red', alerttime)
+            TriggerClientEvent('QBCore:Notify', src, "You don't have enough materials", 'red', 2500)
             TriggerClientEvent('Polar-Crafting:Client:Busy', src)
             break
         end
@@ -123,7 +70,7 @@ RegisterNetEvent('Polar-Crafting:Server:Hitup', function(data)
 
 
     else
-        TriggerClientEvent('QBCore:Notify', src, "You don't have enough materials", 'red', alerttime)
+        TriggerClientEvent('QBCore:Notify', src, "You don't have enough materials", 'red', 2500)
         TriggerClientEvent('Polar-Crafting:Client:Busy', src)
     end
 
