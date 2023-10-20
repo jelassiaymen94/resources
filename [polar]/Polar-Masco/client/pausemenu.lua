0d36c00a64985a129288b6853db8fbe9c629d730
blakeayye <bcoram04@gmail.com>
2023-10-19 21:24:02 -0400

    Revert "1"
    This reverts commit b7823f974bd9d46c7bd76c2c02db0c80af1736f6.

--- [polar]/Polar-Masco/client/pausemenu.lua
+++ [polar]/Polar-Masco/client/pausemenu.lua
@@ -1,4 +1,9 @@
 
+
+Citizen.CreateThread(function()
+   ReplaceHudColourWithRgba(240, 200, 80, 255)
+end)
+
 Citizen.CreateThread(function()
    Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"),'FE_THDR_GTAO', 'Fastro RP')
 end)
