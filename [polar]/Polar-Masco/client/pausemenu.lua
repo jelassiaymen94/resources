b7823f974bd9d46c7bd76c2c02db0c80af1736f6
blakeayye <bcoram04@gmail.com>
2023-10-19 21:22:30 -0400

    1

--- [polar]/Polar-Masco/client/pausemenu.lua
+++ [polar]/Polar-Masco/client/pausemenu.lua
@@ -1,9 +1,4 @@
 
-
-Citizen.CreateThread(function()
-   ReplaceHudColourWithRgba(240, 200, 80, 255)
-end)
-
 Citizen.CreateThread(function()
    Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"),'FE_THDR_GTAO', 'Fastro RP')
 end)
