f6249f5a2ea9ab59c237a942634168d0f1fb9fd8
blakeayye <bcoram04@gmail.com>
2023-10-19 21:51:03 -0400

    1

---
+++ [standalone]/que/config.lua
@@ -0,0 +1,47 @@
+Config = {
+	Default_Prio = 500000, -- This is the default priority value if a discord isn't found
+	AllowedPerTick = 1, -- How many players should we allow to connect at a time?
+	CheckForGhostUsers = 40, -- How many seconds should the script check for ghosts users in the queue?
+	HostDisplayQueue = true,
+	onlyActiveWhenFull = false,
+	Requirements = { -- A player must have the identifier to be allowed into the server
+		Discord = true,
+		Steam = true
+	},
+	WhitelistRequired = false, -- If this option is set to true, a player must have a role in Config.Rankings to be allowed into the server
+	Debug = false,
+	Webhook = '',
+	Displays = {
+		Prefix = 'Fastro Que',
+		ConnectingLoop = { 
+			'🦡🌿🦡🌿🦡🌿',
+			'🌿🦡🌿🦡🌿🦡',
+			'🦡🌿🦡🌿🦡🥦',
+			'🌿🦡🌿🦡🥦🦡',
+			'🦡🌿🦡🥦🦡🥦',
+			'🌿🦡🥦🦡🥦🦡',
+			'🦡🥦🦡🥦🦡🥦',
+			'🥦🦡🥦🦡🥦🦡',
+			'🦡🥦🦡🥦🦡🌿',
+			'🥦🦡🥦🦡🌿🦡',
+			'🦡🥦🦡🌿🦡🌿',
+			'🥦🦡🌿🦡🌿🦡',
+		},
+		Messages = {
+			MSG_CONNECTING = 'You are being connected [{QUEUE_NUM}/{QUEUE_MAX}]: ', -- Default message if they have no discord roles 
+			MSG_CONNECTED = 'You are up! You are being connected now :)',
+			MSG_DISCORD_REQUIRED = 'Your Discord was not detected... You are required to have Discord to play on this server...',
+			MSG_STEAM_REQUIRED = 'Your Steam was not detected... You are required to have Steam to play on this server...',
+			MSG_NOT_WHITELISTED = 'You do not have a Discord role whitelisted for this server... You are not whitelisted.',
+		},
+	},
+}
+
+Config.Rankings = {
+	-- LOWER NUMBER === HIGHER PRIORITY 
+	-- ['roleID'] = {rolePriority, connectQueueMessage},
+	['1011780619258314842'] = {500, "You are being connected (you are not as special as Badger) [{QUEUE_NUM}/{QUEUE_MAX}]:"}, -- Discord User 
+	['Staff'] = {100, "You are being connected (Staff Queue) [{QUEUE_NUM}/{QUEUE_MAX}]:"}, -- Staff 
+	['Admin'] = {50, "You are being connected (Admin Queue) [{QUEUE_NUM}/{QUEUE_MAX}]:"}, -- Admin
+	['Founder'] = {1, "You are being connected (Founder Queue) [{QUEUE_NUM}/{QUEUE_MAX}]:"}, -- Founder
+}
