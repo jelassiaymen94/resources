74683bba78d0d9ccabeb2245bef5ccafbb928eee
blakeayye <bcoram04@gmail.com>
2023-10-20 19:53:37 -0400

    1

--- [jim]/jobgarage/config.lua
+++ [jim]/jobgarage/config.lua
@@ -857,7 +857,8 @@ Config = {
 					rank = {3,4,5},
 					extras = { 1, 2,3,4,5,6,7,8,9,10,11,12,13,14,15 },
 					
-					colors = {0,0},
+					
+					colors = {112,112},
 					--performance = "max",
 					frontbump = 2,
 					spoiler = 1,
