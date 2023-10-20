c152e60efc8be43d27bf060a6380e232d3b25dd8
blakeayye <bcoram04@gmail.com>
2023-10-20 17:26:00 -0400

    1

--- [paid]/mechanic/client/exterior.lua
+++ [paid]/mechanic/client/exterior.lua
@@ -100,7 +100,7 @@ if not policejob() then
 		end
 	end
 else
-	vehicle = getClosest(GetEntityCoords(PlayerPedId())) pushVehicle(vehicle)
+	vehicle = getClosest(GetEntityCoords(PlayerPedId())) -- pushVehicle(vehicle)
 		for i = 1, GetNumVehicleMods(vehicle, tonumber(data.mod)) do
 			if GetVehicleMod(vehicle, tonumber(data.mod)) == (i-1) then txt = Loc[Config.Lan]["common"].current else txt = "" end
 			validMods[i] = { id = (i - 1), name = GetLabelText(GetModTextLabel(vehicle, tonumber(data.mod), (i - 1))), install = txt }
