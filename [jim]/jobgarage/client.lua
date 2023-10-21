38c235f4b8e0f2cc9942f555d06ebfb6278879d5
blakeayye <bcoram04@gmail.com>
2023-10-20 20:02:09 -0400

    1

--- [jim]/jobgarage/client.lua
+++ [jim]/jobgarage/client.lua
@@ -226,6 +226,7 @@ RegisterNetEvent("jim-jobgarage:client:SpawnList", function(data)
 			TriggerServerEvent('Polar-JobGarage:Server:Log')
 			print(data.spawnName)
 			if data.spawnName == 'npolchar' then
+				print('hi')
 				TriggerEvent('Polar-Fitment:Client:WidebodyCharger')
 			end
 		end, data.coords, true)
