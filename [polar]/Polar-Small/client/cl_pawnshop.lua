e704dbb9d4a9637d78a4af26d9cc71f8c6225cdc
blakeayye <bcoram04@gmail.com>
2023-10-21 17:24:06 -0400

    1

--- [polar]/Polar-Small/client/cl_pawnshop.lua
+++ [polar]/Polar-Small/client/cl_pawnshop.lua
@@ -102,4 +102,4 @@ CreateThread(function()
 end)
 
 
-RegisterNetEvent('Pawnshop:Client:Lapa', function() print('loop')  TriggerServerEvent('Pawnshop:Server:SellItems') end)
+RegisterNetEvent('Pawnshop:Client:Lapa', function() TriggerServerEvent('Pawnshop:Server:SellItems') end)
