59fb3ca074f389677e6cd5e7080f8385890be41b
blakeayye <bcoram04@gmail.com>
2023-10-23 00:10:42 -0400

    Update cl_weirdwalk.lua

--- [polar]/Polar-Small/client/cl_weirdwalk.lua
+++ [polar]/Polar-Small/client/cl_weirdwalk.lua
@@ -1,26 +1,9 @@
-local lastFlags = {} -- Table to store the last known flag values
-
-CreateThread(function()
+Citizen.CreateThread(function()
     while true do
-        Wait(1000)  -- Adjust the check interval as needed
-
-        local player = PlayerId()
-        local flagChanged = false
-
-        for i = 0, 457 do
-            local flagValue = GetPedConfigFlag(player, i)
-
-            if lastFlags[i] == nil then
-                lastFlags[i] = flagValue
-            elseif lastFlags[i] ~= flagValue then
-                print('Flag ' .. i .. ' changed to ' .. tostring(flagValue))
-                lastFlags[i] = flagValue
-                flagChanged = true
-            end
-        end
-
-        if flagChanged then
-            -- Handle the case where any flag changed
+        Citizen.Wait(15)
+        local ped = PlayerPedId()
+        if IsPedUsingActionMode(ped) then
+            SetPedUsingActionMode(ped, -1, -1, 1)
         end
     end
 end)
