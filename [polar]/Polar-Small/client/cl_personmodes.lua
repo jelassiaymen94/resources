834de7a509dc3bd7d1bcdeebe9ee3ba659c656e2
blakeayye <bcoram04@gmail.com>
2023-10-22 22:40:30 -0400

    1

--- [polar]/Polar-Small/client/cl_personmodes.lua
+++ [polar]/Polar-Small/client/cl_personmodes.lua
@@ -1,4 +1,4 @@
-local currentViewMode = 0 -- Initial view mode (0 for third-person)
+local currentViewMode = 0
 
 CreateThread(function()
     while true do
@@ -8,11 +8,15 @@ CreateThread(function()
             local newViewMode = GetFollowPedCamViewMode()
 
             if newViewMode == currentViewMode then
-                -- Toggle between view mode 4 (first-person) and view mode 0 (third-person)
                 currentViewMode = (currentViewMode == 4) and 0 or 4
                 SetFollowPedCamViewMode(currentViewMode)
             else
-                currentViewMode = newViewMode
+                if newViewMode >= 1 and newViewMode <= 3 then
+                    currentViewMode = (currentViewMode == 4) and 0 or 4
+                    SetFollowPedCamViewMode(currentViewMode)
+                else
+                    currentViewMode = newViewMode
+                end
             end
         end
     end
