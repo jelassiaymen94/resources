8e0f7089bd77d01616e2b6745641663769c5a5db
blakeayye <bcoram04@gmail.com>
2023-10-22 23:28:04 -0400

    1

--- [polar]/Polar-Small/client/cl_personmodes.lua
+++ [polar]/Polar-Small/client/cl_personmodes.lua
@@ -1,5 +1,3 @@
-local currentViewMode = 0
-
 local keyName = "UNKNOWN" 
 local control = 0 
 
@@ -18,17 +16,12 @@ end)
 CreateThread(function()
     while true do
         if IsControlJustReleased(0, control) then
-            mode()
+            local modes = GetFollowPedCamViewMode()
+            print(modes)
+            if modes ~= 4 then
+                SetFollowPedCamViewMode(4)
+            end
         end
         Wait(10)
     end
 end)
-
-
-function mode()
-    local modes = GetFollowPedCamViewMode()
-    print(modes)
-    if modes ~= 4 then
-        SetFollowPedCamViewMode(4)
-    end
-end
