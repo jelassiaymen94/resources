d7b932f81eb18c457131bb43107dc4b087aac761
blakeayye <bcoram04@gmail.com>
2023-10-23 02:31:56 -0400

    Revert "djbooth converted"
    This reverts commit f9fdcafeed8f9aab0b5a4fdef6e0844a7618eac8.

---
+++ [test]/qb-djbooth/fxmanifest.lua
@@ -0,0 +1,22 @@
+name "QB-DJBooth"
+author "Jimathy"
+version "v1.1"
+description 'QB-DJBooth Edit By Jimathy'
+fx_version "cerulean"
+game "gta5"
+
+client_scripts {
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    '@PolyZone/EntityZone.lua',
+    '@PolyZone/CircleZone.lua',
+    '@PolyZone/ComboZone.lua',
+    'client.lua'
+}
+
+shared_script { 'config.lua' }
+server_script { 'server.lua' }
+
+dependency 'xsound'
+
+lua54 'yes'
