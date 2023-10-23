80ef9c1ba1c9e1b9d014b4418be5682a0afb115d
blakeayye <bcoram04@gmail.com>
2023-10-22 22:35:11 -0400

    Update cl_personmodes.lua

--- [polar]/Polar-Small/client/cl_personmodes.lua
+++ [polar]/Polar-Small/client/cl_personmodes.lua
@@ -1,8 +1,11 @@
+
+local currentViewMode = 0 
+
+
 CreateThread(function()
-    local currentViewMode = 0  -- Initial view mode (0 for third-person)
-    local player = PlayerPedId()
     while true do
         Wait(10)
+        local player = PlayerPedId()
         if DoesEntityExist(player) and not IsEntityDead(player) then
             local newViewMode = GetFollowPedCamViewMode()
             if newViewMode ~= currentViewMode then
