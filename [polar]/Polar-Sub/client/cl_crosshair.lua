7d443bb2481ca2b2841d39167a07f055b1a2b3d4
blakeayye <bcoram04@gmail.com>
2023-10-23 01:35:03 -0400

    Update cl_crosshair.lua

--- [polar]/Polar-Sub/client/cl_crosshair.lua
+++ [polar]/Polar-Sub/client/cl_crosshair.lua
@@ -1,10 +1,9 @@
 
 local on = true
 
-
-local centerX, centerY = 0.5, 0.5 -- Middle of the screen
-local circleRadius = 25.0 -- Adjust the radius as needed
+local circleRadius = 50.0 -- Adjust the radius as needed
 local circleColor = {255, 255, 255, 255} -- RGBA color values
+local circleSegments = 360 -- Number of line segments to approximate the circle
 
 
 
@@ -40,5 +39,15 @@ end)
 
 
 function drawCrosshair()
-	local sprite = DrawCircle(centerX, centerY, 0, 5, circleColor[1], circleColor[2], circleColor[3], circleColor[4])
+	local centerX, centerY = GetActiveScreenResolution() / 2, GetActiveScreenResolution() / 2
+	local step = 2.0 * math.pi / circleSegments
+
+	for i = 1, circleSegments do
+		local x1 = circleRadius * math.cos(step * (i - 1)) + centerX
+		local y1 = circleRadius * math.sin(step * (i - 1)) + centerY
+		local x2 = circleRadius * math.cos(step * i) + centerX
+		local y2 = circleRadius * math.sin(step * i) + centerY
+
+		DrawLine(x1, y1, x2, y2, circleColor[1], circleColor[2], circleColor[3], circleColor[4])
+	end
 end
