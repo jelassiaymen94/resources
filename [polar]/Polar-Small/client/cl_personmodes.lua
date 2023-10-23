b726d9c9a60887f82152be8bcd3a96a017e72273
blakeayye <bcoram04@gmail.com>
2023-10-22 22:47:22 -0400

    1

--- [polar]/Polar-Small/client/cl_personmodes.lua
+++ [polar]/Polar-Small/client/cl_personmodes.lua
@@ -6,16 +6,16 @@ CreateThread(function()
         local player = PlayerPedId()
         if DoesEntityExist(player) and not IsEntityDead(player) then
             local newViewMode = GetFollowPedCamViewMode()
-
-            if newViewMode == currentViewMode then
-                currentViewMode = (currentViewMode == 4) and 0 or 4
-                SetFollowPedCamViewMode(currentViewMode)
-            else
-                if newViewMode >= 1 and newViewMode <= 3 then
-                    currentViewMode = (currentViewMode == 4) and 0 or 4
-                    SetFollowPedCamViewMode(currentViewMode)
-                else
-                    currentViewMode = newViewMode
+            if newViewMode ~= 0 then
+                if newViewMode == 0 then
+                elseif newViewMode ~= 4 then
+                    if currentViewMode == 4 then 
+                        SetFollowPedCamViewMode(0)
+                        currentViewMode = 0
+                    elseif currentViewMode == 0 then
+                        SetFollowPedCamViewMode(4)
+                        currentViewMode = 4
+                    end  
                 end
             end
         end
