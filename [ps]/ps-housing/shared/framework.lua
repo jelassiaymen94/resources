54c16292195967c51a19c4737b5bac19f15bb3ee
blakeayye <bcoram04@gmail.com>
2023-10-24 21:01:10 -0400

    1

--- [ps]/ps-housing/shared/framework.lua
+++ [ps]/ps-housing/shared/framework.lua
@@ -8,22 +8,14 @@ for i = 1, #Config.PoliceJobNames do
 end
 
 if IsDuplicityVersion() then
-    Framework.ox = {}
     Framework.qb = {}
 
-    function Framework.ox.Notify(src, message, type)
-        type = type == "inform" and "info" or type
-        TriggerClientEvent("ox_lib:notify", src, {title="Property", description=message, type=type})
-    end
-
+  
     function Framework.qb.Notify(src, message, type)
         type = type == "info" and "primary" or type
         TriggerClientEvent('QBCore:Notify', src, message, type)
     end
 
-    function Framework.ox.RegisterInventory(stash, label, stashConfig)
-        exports.ox_inventory:RegisterStash(stash, label, stashConfig.slots, stashConfig.maxweight, nil)
-    end
 
     function Framework.qb.RegisterInventory(stash, label, stashConfig)
         -- Used for ox_inventory compat
@@ -35,10 +27,6 @@ if IsDuplicityVersion() then
         end
     end
     
-    function Framework.ox.SendLog(message)
-            -- noop
-    end
-
     return
 end
 
@@ -130,6 +118,7 @@ Framework.qb = {
                         end,
                     },
                 },
+                distance = 1.5
             }
         )
 
@@ -172,7 +161,8 @@ Framework.qb = {
                         return PoliceJobs[jobName] and gradeAllowed and onDuty
                     end,
                 },
-            }
+            },
+            distance = 1.5
         })
     end,
 
@@ -202,6 +192,7 @@ Framework.qb = {
                         icon = "fas fa-bell",
                     },
                 },
+                distance = 1.5
             }
         )
 
@@ -229,6 +220,7 @@ Framework.qb = {
                         icon = "fas fa-right-from-bracket",
                     },
                 },
+                distance = 1.5
             }
         )
 
@@ -239,21 +231,6 @@ Framework.qb = {
         exports["qb-target"]:RemoveZone(targetName)
     end,
 
-    AddRadialOption = function(id, label, icon, _, event, options)
-        exports['qb-radialmenu']:AddOption({
-            id = id,
-            title = label,
-            icon = icon,
-            type = 'client',
-            event = event,
-            shouldClose = true,
-            options = options
-        }, id)
-    end,
-
-    RemoveRadialOption = function(id)
-        exports['qb-radialmenu']:RemoveOption(id)
-    end,
 
     AddTargetEntity = function (entity, label, icon, action)
         exports["qb-target"]:AddTargetEntity(entity, {
@@ -264,6 +241,7 @@ Framework.qb = {
                     action = action,
                 },
             },
+            distance = 1.5
         })
     end,
 
@@ -274,208 +252,5 @@ Framework.qb = {
     OpenInventory = function (stash, stashConfig)
         TriggerServerEvent("inventory:server:OpenInventory", "stash", stash, stashConfig)
         TriggerEvent("inventory:client:SetCurrentStash", stash)
-    end,
-}
-
-Framework.ox = {
-    Notify = function(message, type)
-        type = type == "inform" and "info" or type
-        
-        lib.notify({
-            title = 'Property',
-            description = message,
-            type = type
-        })
-    end,
-
-    AddEntrance = function (coords, size, heading, propertyId, enter, raid, showcase, showData, _)
-        local property_id = propertyId
-
-        local handler = exports.ox_target:addBoxZone({
-            coords = vector3(coords.x, coords.y, coords.z),
-            size = vector3(size.y, size.x, size.z),
-            rotation = heading,
-            debug = Config.DebugMode,
-            options = {
-                {
-                    label = "Enter Property",
-                    icon = "fas fa-door-open",
-                    onSelect = enter,
-                    canInteract = function()
-                        local property = Property.Get(property_id)
-                        return property.has_access or property.owner
-                    end,
-                },
-                {
-                    label = "Showcase Property",
-                    icon = "fas fa-eye",
-                    onSelect = showcase,
-                    canInteract = function()
-                        -- local property = Property.Get(property_id)
-                        -- if property.propertyData.owner ~= nil then return false end -- if its owned, it cannot be showcased
-                        
-                        local job = PlayerData.job
-                        local jobName = job.name
-
-                        return jobName == Config.RealtorJobName
-                    end,
-                },
-                {
-                    label = "Property Info",
-                    icon = "fas fa-circle-info",
-                    onSelect = showData,
-                    canInteract = function()
-                        local job = PlayerData.job
-                        local jobName = job.name
-                        local onDuty = job.onduty
-                        return jobName == Config.RealtorJobName and onDuty
-                    end,
-                },
-                {
-                    label = "Ring Doorbell",
-                    icon = "fas fa-bell",
-                    onSelect = enter,
-                    canInteract = function()
-                        local property = Property.Get(property_id)
-                        return not property.has_access and not property.owner
-                    end,
-                },
-                {
-                    label = "Raid Property",
-                    icon = "fas fa-building-shield",
-                    onSelect = raid,
-                    canInteract = function()
-                        local job = PlayerData.job
-                        local jobName = job.name
-                        local gradeAllowed = tonumber(job.grade.level) >= Config.MinGradeToRaid
-                        local onDuty = job.onduty
-
-                        return PoliceJobs[jobName] and onDuty and gradeAllowed
-                    end,
-                },
-            },
-        })
-
-        return handler
-    end,
-
-    AddApartmentEntrance = function (coords, size, heading, apartment, enter, seeAll, seeAllToRaid, _)        
-        local handler = exports.ox_target:addBoxZone({
-            coords = vector3(coords.x, coords.y, coords.z),
-            size = vector3(size.y, size.x, size.z),
-            rotation = heading,
-            debug = Config.DebugMode,
-            options = {
-                {
-                    label = "Enter Apartment",
-                    onSelect = enter,
-                    icon = "fas fa-door-open",
-                    canInteract = function()
-                        local apartments = ApartmentsTable[apartment].apartments
-                        return hasApartment(apartments)
-                    end,
-                },
-                {
-                    label = "See all apartments",
-                    onSelect = seeAll,
-                    icon = "fas fa-circle-info",
-                },
-                {
-                    label = "Raid Apartment",
-                    onSelect = seeAllToRaid,
-                    icon = "fas fa-building-shield",
-                    canInteract = function()
-                        local job = PlayerData.job
-                        local jobName = job.name
-                        local gradeAllowed = tonumber(job.grade.level) >= Config.MinGradeToRaid
-                        local onDuty = job.onduty
-
-                        return PoliceJobs[jobName] and onDuty and gradeAllowed
-                    end,
-                },
-            },
-        })
-
-        return handler
-    end,
-
-    AddDoorZoneInside = function (coords, size, heading, leave, checkDoor)
-        local handler = exports.ox_target:addBoxZone({
-            coords = vector3(coords.x, coords.y, coords.z), --z = 3.0
-            size = vector3(size.y, size.x, size.z),
-            rotation = heading,
-            debug = Config.DebugMode,
-            options = {
-                {
-                    name = "leave",
-                    label = "Leave Property",
-                    onSelect = leave,
-                    icon = "fas fa-right-from-bracket",
-                },
-                {
-                    name = "doorbell",
-                    label = "Check Door",
-                    onSelect = checkDoor,
-                    icon = "fas fa-bell",
-                },
-            },
-        })
-
-        return handler
-    end,
-
-    AddDoorZoneInsideTempShell = function (coords, size, heading, leave)
-        local handler = exports.ox_target:addBoxZone({
-            coords = vector3(coords.x, coords.y, coords.z), --z = 3.0
-            size = vector3(size.y, size.x, size.z),
-            rotation = heading,
-            debug = Config.DebugMode,
-            options = {
-                {
-                    name = "leave",
-                    label = "Leave",
-                    onSelect = leave,
-                    icon = "fas fa-right-from-bracket",
-                },
-            },
-        })
-        print("made")
-        return handler
-    end,
-
-    RemoveTargetZone = function (handler)
-        exports.ox_target:removeZone(handler)
-    end,
-
-    AddRadialOption = function(id, label, icon, fn)
-        lib.addRadialItem({
-            id = id,
-            icon = icon,
-            label = label,
-            onSelect = fn,
-        })
-    end,
-
-    RemoveRadialOption = function(id)
-        lib.removeRadialItem(id)
-    end,
-
-    AddTargetEntity = function (entity, label, icon, action)
-        exports.ox_target:addLocalEntity(entity, {
-            {
-                name = label,
-                label = label,
-                icon = icon,
-                onSelect = action,
-            },
-        })
-    end,
-
-    RemoveTargetEntity = function (entity)
-        exports.ox_target:removeLocalEntity(entity)
-    end,
-
-    OpenInventory = function (stash, stashConfig)
-        exports.ox_inventory:openInventory('stash', stash)
     end,
 }
